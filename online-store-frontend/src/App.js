aac01021a45b89dbb883238ad5060725900cfba1
lenovo <roxi_nicoleta2104@yahoo.com>
2024-02-27 22:02:05 +0200

    footer

--- online-store-frontend/src/App.js
+++ online-store-frontend/src/App.js
@@ -6,6 +6,7 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -13,16 +14,17 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Shop />}/>
-          <Route path="/men" element={<ShopCategory category="men" />}/>
-          <Route path="/women" element={<ShopCategory category="women" />}/>
-          <Route path="/kids" element={<ShopCategory category="kids" />}/>
+          <Route path="/" element={<Shop />} />
+          <Route path="/men" element={<ShopCategory category="men" />} />
+          <Route path="/women" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
           <Route path="/product" element={<Product />}>
-            <Route path=":productId" element={<Product />}/>
+            <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path="/cart" element={<Cart />}/>
-          <Route path="/login" element={<LoginSignup />}/>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
