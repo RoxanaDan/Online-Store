e4f985d462ade39c37840d24705e36f41c52663b
roxanadan <roxi_nicoleta2104@yahoo.com>
2024-03-03 20:01:03 +0200

    WIP

--- online-store-frontend/src/Pages/Product.jsx
+++ online-store-frontend/src/Pages/Product.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
 
 export const Product = () => {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const productId = useParams();
+  return <div>
+
+  </div>;
 };
 
 export default Product;
