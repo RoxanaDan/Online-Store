67c6d574a1fb6de86fd28f88f8ce00a8aa231ec1
lenovo <roxi_nicoleta2104@yahoo.com>
2024-04-01 22:33:09 +0300

    configured backend project

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection with MongoDB
+mongoose.connect("mongodb+srv://roxinicoleta2104:q2xKZ61YH7GVD8rU@cluster0.9wojvo1.mongodb.net/online-store")
+
+// API Creation
+
+app.get("/", (req, res) => {
+    res.send("Express App is running")
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server running on port " + port)
+    } else {
+        console.log("Error: " + error)
+    }
+
+})
