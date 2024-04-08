0741b9aec9ef31967f9311bf6560376dc2683a08
lenovo <roxi_nicoleta2104@yahoo.com>
2024-04-08 23:24:24 +0300

    CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -75,21 +75,47 @@ const Product = mongoose.model("Prodct", {
 })
 
 app.post('/add-product', async (req, res) => {
+    let products = await Product.find({});
+    let id;
+    if (products.length > 0) {
+        last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
+
+    }
+    else {
+        id = 1;
+    }
+
     const product = new Product({
-        id: req.body.id,
+        id: id,
         name: req.body.name,
         image: req.body.image,
         category: req.body.category,
         new_price: req.body.new_price,
         old_price: req.body.old_price
     })
-    console.log(product);
     await product.save();
-    console.log('saved');
     res.json({
         succes: true,
         name: req.body.name
     })
+})
+
+// Creating API for deleting products
+app.post('/remove-product', async (req, res) => {
+    await Product.findOneAndDelete({ id: req.body.id });
+    res.json({
+        succes: true,
+        name: req.body.name
+    })
+})
+
+// Creating API for getting all products
+app.get('/all-products', async (req, res) => {
+    let products = await Product.find({});
+    console.log("all products fetched");
+    res.send(products);
 })
 
 app.listen(port, (error) => {
