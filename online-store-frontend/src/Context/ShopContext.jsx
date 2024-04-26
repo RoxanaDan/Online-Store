785748792430368d5919943cdceb6755efda5799
lenovo <roxi_nicoleta2104@yahoo.com>
2024-04-27 00:28:03 +0300

    List and remove product admin

--- online-store-frontend/src/Context/ShopContext.jsx
+++ online-store-frontend/src/Context/ShopContext.jsx
@@ -44,12 +44,12 @@ const ShopContextProvider = (props) => {
   const getTotalCartItems = () => {
     let totalItem = 0;
     for (const item in cartItems) {
-      if(cartItems[item] > 0) {
-        totalItem += cartItems[item]
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
       }
     }
     return totalItem;
-  }
+  };
 
   const contextValue = {
     getTotalCartItems,
