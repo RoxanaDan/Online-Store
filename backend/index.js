b471fc7616e5bebc9bec7a45194ec060e817ffab
lenovo <roxi_nicoleta2104@yahoo.com>
2024-04-27 11:24:15 +0300

    sign up function

--- backend/index.js
+++ backend/index.js
@@ -118,6 +118,82 @@ app.get('/all-products', async (req, res) => {
     res.send(products);
 })
 
+// Schema for creating User Model
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    cartData: {
+        type: Object
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+// Endpoint for user register
+app.post('/sign-up', async (req, res) => {
+    let check = await User.findOne({ email: req.body.email });
+    if (check) {
+        res.status(400).json({ success: false, errors: "existing user found with same email address" })
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+
+    }
+    const user = new User({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart
+    })
+
+    await user.save();
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom')
+    res.json({ success: true, token })
+})
+
+// Endpoint for user login
+app.post('/login', async (req, res) => {
+    let user = await User.findOne({
+        email: req.body.email
+    });
+    if (user) {
+        const passwordCompare = req.body.password === user.password;
+        if (passwordCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_ecom');
+            res.json({ success: true, token })
+        }
+        else {
+            res.json({ success: false, errors: "Wrong Password" })
+        }
+    }
+    else {
+        res.json({ success: false, errors: "Wrong email" })
+    }
+})
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server running on port " + port)
--- online-store-frontend/src/Pages/LoginSignup.jsx
+++ online-store-frontend/src/Pages/LoginSignup.jsx
@@ -1,23 +1,113 @@
-import React from "react";
-import './CSS/LoginSignup.css'
+import React, { useState } from "react";
+import "./CSS/LoginSignup.css";
 
 export const LoginSignup = () => {
-  return <div className="loginsignup">
-    <div className="loginsignup-container">
-      <h1>Sign Up</h1>
-      <div className="loginsignup-fields">
-        <input type="text" placeholder="Your name" />
-        <input type="email" placeholder="Email address" />
-        <input type="password" placeholder="Password" />
-      </div>
-      <button>Continue</button>
-      <p className="loginsignup-login">Already have an account? <span>Log in here</span></p>
-      <div className="loginsignup-agree">
-        <input type="checkbox" name="" id="" />
-        <p>By continuing, I agree to the terms of use & privacy policy</p>
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const login = async () => {
+    console.log("Login func", formData);
+  };
+
+  const signup = async () => {
+    console.log("signup func", formData);
+    let responseData;
+    await fetch("http://localhost:4000/sign-up", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        responseData = data;
+      });
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.errors);
+    }
+  };
+
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>{state}</h1>
+        <div className="loginsignup-fields">
+          {state === "Sign Up" ? (
+            <input
+              name="username"
+              value={formData.username}
+              onChange={changeHandler}
+              type="text"
+              placeholder="Your name"
+            />
+          ) : (
+            <></>
+          )}
+          <input
+            name="email"
+            value={formData.email}
+            onChange={changeHandler}
+            type="email"
+            placeholder="Email address"
+          />
+          <input
+            name="password"
+            value={formData.password}
+            onChange={changeHandler}
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <button
+          onClick={() => {
+            state === "Login" ? login() : signup();
+          }}
+        >
+          Continue
+        </button>
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account?{" "}
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account?{" "}
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+            >
+              Click here
+            </span>
+          </p>
+        )}
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use & privacy policy</p>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default LoginSignup;
