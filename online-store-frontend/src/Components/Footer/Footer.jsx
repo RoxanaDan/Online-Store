0080b1a51dd4da2c67fea2f35547779dde6bdd87
lenovo <roxi_nicoleta2104@yahoo.com>
2024-06-12 19:34:18 +0300

    fix

--- online-store-frontend/src/Components/Footer/Footer.jsx
+++ online-store-frontend/src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     <div className="footer">
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>SHOPPER</p>
+        <p>Trendy Style</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
@@ -32,7 +32,7 @@ export const Footer = () => {
       </div>
       <div className="footer-copyright">
         <hr />
-        <p>Copyright @2023 - All Right Reserved</p>
+        <p>Copyright @2024 - All Right Reserved</p>
       </div>
     </div>
   );
--- online-store-frontend/src/Components/Navbar/Navbar.jsx
+++ online-store-frontend/src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SHOPPER</p>
+        <p>Trendy Style</p>
       </div>
       <img
         className="nav-dropdown"
